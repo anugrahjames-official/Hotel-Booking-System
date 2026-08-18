31da2bb4491d860c45613c2d5e48be20fc3cddb0
asitharaj <asitharaj787@gmail.com>
2026-08-18 21:43:30 +0530

    Implemented Booking.java (#7)

--- src/hotel/model/Booking.java
+++ src/hotel/model/Booking.java
@@ -1,5 +1,32 @@
 package hotel.model;
 
-public class Booking {
+// Represents a room reservation made by a guest
 
+public class Booking {
+    private int bookingId;
+    private int guestId;
+    private int roomNo;
+    private String checkIn;
+    private String checkOut;
+    private double bill;
+
+    // Constructor
+    public Booking(int bookingId, int guestId, int roomNo,
+                   String checkIn, String checkOut, double bill) {
+        this.bookingId = bookingId;
+        this.guestId = guestId;
+        this.roomNo = roomNo;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.bill = bill;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking " + bookingId +
+               " | Guest ID: " + guestId +
+               " | Room No: " + roomNo +
+               " | Dates: " + checkIn + " to " + checkOut +
+               " | Bill: Rs." + bill;
+    }
 }
